6bf753b332589f55bc690e007df928cb2aca1ff0
Robert Całka <calka.rob@gmail.com>
2018-09-22 10:36:13 +0200

    init code to start

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+
+window.addEventListener("load", function(event) { 
+    
+    const width = 500;
+    const height = 450;
+   
+    const body:HTMLElement = document.querySelector('body');
+    
+    
+});
+
+
+
